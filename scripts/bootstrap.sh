446586ac7ca7099aeb3e7f6d1683f50140af668f
Marc Miller <marc.miller@cse-corp.com>
2021-10-17 14:20:44 -0600

    add initial scripts

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Exit on any non-zero exit codes
+set -e
+
+# Default packages to install
+PACKAGE_LIST=( 
+  apt-transport-https 
+  brave-browser
+  ca-certificates
+  code 
+  curl
+  fzf 
+  git 
+  gnupg
+  kitty 
+  mattermost-desktop
+  maven
+  neovim
+  nodejs
+  npm
+  python3
+  slack
+  software-properties-common
+  wget
+  zsh 
+  )
+
+# Path to the script being executed
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Source all of our helper scripts
+source $SCRIPT_DIR/functions.sh
+
+function main {
+  case "${1}" in 
+    -v | --verbose)
+      printf "Running script in verbose mode\n"
+      set -x
+      ;;
+
+    *)
+      printf "Running script in regular mode\n"
+    ;;
+  esac
+
+  setup
+  installPackages
+}
+
+main ${@}
---
+++ scripts/functions.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+checkOs() {
+  source /etc/lsb-release
+
+  VALID_RELEASES=( 20.04 21.04 )
+  printf "\nChecking OS and version...\n"
+
+  if [[ ! " $DISTRIB_ID " =~ " Ubuntu " ]]; then
+    printf "  Sorry, this script is only for Ubuntu\n"
+    exit 1
+  fi
+
+  if [[ ! " ${VALID_RELEASES[*]} " =~ " $DISTRIB_RELEASE " ]]; then
+    printf "  Sorry, only Ubuntu %s are supported\n"
+    exit 1
+  fi
+
+  printf "  Found %s\n" "$DISTRIB_DESCRIPTION"
+}
+
+function setup() {
+  checkOs
+  getElevatedPermissions
+  queryLaptop
+  queryBluetooth
+}
+
+function queryLaptop() {
+  printf "\nAre you currently setting up a laptop? [yN]"
+  old_stty_cfg=$(stty -g)
+  stty raw -echo ; IS_LAPTOP=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
+  if printf "$IS_LAPTOP" | grep -iq "^y" ;then
+    printf "\n  Adding tlp to package list\n"
+    PACKAGE_LIST=( ${PACKAGE_LIST[@]} tlp )
+  else
+    printf "\n  Skipping tlp setup\n"
+  fi
+}
+
+function queryBluetooth() {
+  printf "\nWill you be using a Bluetooth headset? [yN]"
+  old_stty_cfg=$(stty -g)
+  stty raw -echo ; USE_BLUETOOTH=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
+  if printf "$USE_BLUETOOTH" | grep -iq "^y" ;then
+    printf "\n  Adding pipewire-debian upstream ppa\n"
+    sudo sh -c "add-apt-repository -y ppa:pipewire-debian/pipewire-upstream" 2>1 /dev/null
+    printf "\n  Adding pipewire and dependencies to package list\n"
+    PACKAGE_LIST=( ${PACKAGE_LIST[@]} pipewire gstreamer1.0-pipewire libspa-0.2-{bluetooth,jack} pipewire-audio-client-libraries )
+  else
+    printf "\n  Skipping tlp setup\n"
+  fi
+}
+
+function getElevatedPermissions() {
+  printf "\nRequesting elevated permissions to install software...\n"
+  sudo -v
+  while true; do 
+    sudo -n true;
+    sleep 60;
+    kill -0 "$$" || exit;
+  done 2>/dev/null &
+}
+
+function installPackages() {
+  printf "Installing packages...\n"
+  for package in ${PACKAGE_LIST[@]}
+  do
+    printf "  -> %s\n" $package
+  done
+}
