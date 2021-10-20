23db92d1597b8c39ae25711315c8faccd7839f8d
Marc Miller <marc.miller@cse-corp.com>
2021-10-20 10:03:59 -0400

    install stage works nicely
    Adds Sources/Keys for a few apps, then installs with Apt-get, and then
    grabs manual install .deb files, and installs those.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -43,7 +43,7 @@ function install_apt {
 
   for PACKAGE in ${PACKAGE_LIST[@]}
   do
-    PKG_INSTALLED=$(dpkg-query -W -f='${db:Status-Status}' $PACKAGE 2>&1| grep -c "installed"; return 0)
+    PKG_INSTALLED=$(dpkg-query -W -f='${Status}\n' $PACKAGE 2>&1| grep -c "ok installed"; return 0)
     if [ $PKG_INSTALLED -ge 1 ]; then
       printf " -> %s already installed, skipping...\n" "${PACKAGE}"
     else
@@ -71,6 +71,7 @@ function install_apt {
 
       printf " -> Installing debs...\n"
       for DEB in *.deb; do
+        printf "   -> Installing %s deb...\n" "$DEB"
         sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get install -qq /tmp/debs/${DEB}" < /dev/null > /dev/null
       done
       jobsDone
